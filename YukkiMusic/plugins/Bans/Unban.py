7201d073cf383d4a5fc30c6943fed4a69319bfb0
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-08 10:47:02 +0530

    Create Unban.py

---
+++ YukkiMusic/plugins/Bans/Unban.py
@@ -0,0 +1,29 @@
+from YukkiMusic import app
+from pyrogram import filters
+from .admin_check import Authorized_user
+
+
+
+@app.on_message(filters.command("unban") & ~filters.edited & ~filters.private)
+@
+async def unban_func(_, message: Message):
+    # we don't need reasons for unban, also, we
+    # don't need to get "text_mention" entity, because
+    # normal users won't get text_mention if the user
+    # they want to unban is not in the group.
+    reply = message.reply_to_message
+
+    if reply and reply.sender_chat and reply.sender_chat != message.chat.id:
+        return await message.reply_text("You cannot unban a channel")
+
+    if len(message.command) == 2:
+        user = message.text.split(None, 1)[1]
+    elif len(message.command) == 1 and reply:
+        user = message.reply_to_message.from_user.id
+    else:
+        return await message.reply_text(
+            "Provide a username or reply to a user's message to unban."
+        )
+    await message.chat.unban_member(user)
+    umention = (await app.get_users(user)).mention
+    await message.reply_text(f"Unbanned! {umention}")
