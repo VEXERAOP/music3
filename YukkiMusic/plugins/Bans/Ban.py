ba8b1c7c3544b2bfefe4fe1c5643acfc881995cb
ABHISHEK THAKUR <99659192+Nobita-XD@users.noreply.github.com>
2022-06-08 11:10:42 +0530

    Create Ban.py

---
+++ YukkiMusic/plugins/Bans/Ban.py
@@ -0,0 +1,62 @@
+from YukkiMusic import app
+from pyrogram import filters
+from .admin_check import authorized_users_only
+from pyrogram.types import Message
+from .extract import extract
+
+@app.on_message(filters.command("ban") & ~filters.edited & ~filters.private)
+@authorized_users_only
+async def banFunc(_, message: Message):
+    user_id, reason = await extract(message, sender_chat=True)
+
+    if not user_id:
+        return await message.reply_text("I can't find that user.")
+    if user_id == BOT_ID:
+        return await message.reply_text(
+            "I can't ban myself, i can leave if you want."
+        )
+    if user_id in SUDOERS:
+        return await message.reply_text(
+            "You Wanna Ban The Elevated One?, RECONSIDER!"
+        )
+    if user_id in (await list_admins(message.chat.id)):
+        return await message.reply_text(
+            "I can't ban an admin, You know the rules, so do i."
+        )
+
+    try:
+        mention = (await app.get_users(user_id)).mention
+    except IndexError:
+        mention = (
+            message.reply_to_message.sender_chat.title
+            if message.reply_to_message
+            else "Anon"
+        )
+
+    msg = (
+        f"**Banned User:** {mention}\n"
+        f"**Banned By:** {message.from_user.mention if message.from_user else 'Anon'}\n"
+    )
+    if message.command[0][0] == "d":
+        await message.reply_to_message.delete()
+    if message.command[0] == "tban":
+        split = reason.split(None, 1)
+        time_value = split[0]
+        temp_reason = split[1] if len(split) > 1 else ""
+        temp_ban = await time_converter(message, time_value)
+        msg += f"**Banned For:** {time_value}\n"
+        if temp_reason:
+            msg += f"**Reason:** {temp_reason}"
+        try:
+            if len(time_value[:-1]) < 3:
+                await message.chat.ban_member(user_id, until_date=temp_ban)
+                await message.reply_text(msg)
+            else:
+                await message.reply_text("You can't use more than 99")
+        except AttributeError:
+            pass
+        return
+    if reason:
+        msg += f"**Reason:** {reason}"
+    await message.chat.ban_member(user_id)
+    await message.reply_text(msg)
